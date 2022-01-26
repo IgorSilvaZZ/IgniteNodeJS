39dea6e54bcf7c430b2ac4a1ed4e53f9857c71b0
IgorSilvaZZ <igor06silva12@gmail.com>
2022-01-26 20:57:13 -0300

    Inital module 4

---
+++ Module4/src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -0,0 +1,43 @@
+import { getRepository, Repository } from "typeorm";
+import { Specification } from "../../entities/Specification";
+import {
+    ISpecificationRepository,
+    ICreateSpecificationDTO,
+} from "../ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+    private repository: Repository<Specification>;
+
+    private constructor() {
+        this.repository = getRepository(Specification);
+    }
+
+    /* public static getInstance(): SpecificationRepository {
+        if (!SpecificationRepository.INSTANCE) {
+            SpecificationRepository.INSTANCE = new SpecificationRepository();
+        }
+
+        return SpecificationRepository.INSTANCE;
+    } */
+
+    async create({
+        description,
+        name,
+    }: ICreateSpecificationDTO): Promise<void> {
+        const specification = this.repository.create({
+            description,
+            name,
+        });
+
+        await this.repository.save(specification);
+    }
+
+    async findByName(name: string): Promise<Specification> {
+        const specification = await this.repository.findOne({
+            name,
+        });
+        return specification;
+    }
+}
+
+export { SpecificationRepository };
