d2415eba7d5563b014aca15118b6a1f9af8db991
IgorSilvaZZ <igor06silva12@gmail.com>
2022-04-25 20:18:16 -0300

    Add Devolution and ListByUserRental

--- Module5/src/modules/rentals/dtos/ICreateRentalDTO.ts
+++ Module5/src/modules/rentals/dtos/ICreateRentalDTO.ts
@@ -3,6 +3,7 @@ export interface ICreateRentalDTO {
     user_id: string;
     car_id: string;
     start_date?: Date;
+    end_date?: Date;
     expected_return_date: Date;
     total?: number;
     created_at?: Date;
